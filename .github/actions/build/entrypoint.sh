8cd0f934ff9f373a4c80fdefb5fd795ea43e9279
Laurent Gil <5628764+angegar@users.noreply.github.com>
2022-10-06 13:09:29 +0000

    feat: rework entrypoint to better build marp
    Signed-off-by: Laurent Gil <5628764+angegar@users.noreply.github.com>

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+build_marp () {
+    source="$1"
+    destination="$2"
+
+    # marp "$source/README.md" -o "$destination/README.html"
+    marp -I "$source" -o "$destination"
+    if [ -d "$source/img" ]; then 
+        cp -r "$source/img" "$destination/"
+    fi
+}
+
+# ==================================================================
+
+
 ## Build Kubernetes slides
-marp slidedecks/kubernetes-overview/README.md -o docs/Kubernetes/kubernetes-overview/README.html
-cp -r slidedecks/kubernetes-overview/img docs/Kubernetes/kubernetes-overview/
+build_marp slidedecks/kubernetes-overview docs/Kubernetes/kubernetes-overview
 
 ## Build CDK slides
-marp slidedecks/cdk/README.md -o docs/cdk/README.html
-cp -r slidedecks/cdk/img docs/cdk/
-
+build_marp slidedecks/cdk docs/cdk/
 
 ## Build web site - must be the latest task
 mkdocs build
 export MKDOCS_GIT_COMMITTERS_APIKEY=$GH_TOKEN
 mkdocs gh-deploy --force
-# marp ${MARP_INPUT_PATH} -o ${GITHUB_WORKSPACE}/${MARP_OUTPUT_PATH}
-# cp -r ${MARP_INPUT_PATH}/img ${GITHUB_WORKSPACE}/${MARP_OUTPUT_PATH}/
-# mkdocs build
-# ls -al
