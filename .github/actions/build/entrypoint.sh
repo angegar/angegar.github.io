4ace351c1751458d488265db496825b611189297
Laurent Gil <5628764+angegar@users.noreply.github.com>
2022-10-06 12:53:34 +0000

    feat(cdk): build pages + add imags
    Signed-off-by: Laurent Gil <5628764+angegar@users.noreply.github.com>

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-## Build web site
+## Build Kubernetes slides
 marp slidedecks/kubernetes-overview/README.md -o docs/Kubernetes/kubernetes-overview/README.html
 cp -r slidedecks/kubernetes-overview/img docs/Kubernetes/kubernetes-overview/
+
+## Build CDK slides
+marp slidedecks/cdk/README.md -o docs/cdk/README.html
+cp -r slidedecks/cdk/img docs/cdk/
+
+
+## Build web site - must be the latest task
 mkdocs build
 export MKDOCS_GIT_COMMITTERS_APIKEY=$GH_TOKEN
 mkdocs gh-deploy --force
